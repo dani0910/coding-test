44b9cb8e5dcdc294dfe9dfa3da5ee6e14eafe02a
잉당웅 <145967279+dani0910@users.noreply.github.com>
2025-11-21 00:55:28 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,19 @@
+function solution(num){
+            let n = num;
+            let count = 0;
+            while(n !== 1){
+                if(n%2 === 0){
+                    n = n / 2;
+                }else{
+                    n = n * 3 + 1;
+                }
+
+                count ++;
+                
+                if(count >= 500){
+                    return -1
+                }
+                
+            }
+            return count;
+        }
