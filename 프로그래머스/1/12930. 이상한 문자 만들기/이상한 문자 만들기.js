79a766117b5ec3e93925315ae8e9fa8c05671277
잉당웅 <145967279+dani0910@users.noreply.github.com>
2026-01-19 03:16:40 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.21 ms, Memory: 33.2 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,19 +1,13 @@
 function solution(s) {
-    let arr = s.split(" ");
-    console.log(arr);
-    for(let i=0; i<arr.length; i++){
-        let word = arr[i].split("")
-        //문자열 자체로는 인덱스로 수정이 불가하기 때문에 임시 배열로 바꿔준다
-        console.log(word);
+    let new_s = s.split(" ");
+    
+    for (let i=0; i<new_s.length; i++){
+        let word = [...new_s[i]];
+        
         for(let j=0; j<word.length; j++){
-            console.log(word[j])
-            word[j] = (j%2===0) ? word[j].toUpperCase() : word[j].toLowerCase();
-            console.log(word[j])
+            word[j] = j%2==0 ? word[j].toUpperCase() : word[j].toLowerCase();
         }
-        arr[i] = word.join("")
-        console.log(arr[i])
+        new_s[i] = word.join("");
     }
-    console.log(arr)
-    console.log(arr.join(" "))
-    return arr.join(" ")
+    return new_s.join(" ");
 }
