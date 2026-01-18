f303cca7f38597f5d8c3b03db396c395766c0364
잉당웅 <145967279+dani0910@users.noreply.github.com>
2026-01-19 03:59:02 +0900

    [level 1] Title: 약수의 합, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.js
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.js
@@ -1,15 +1,18 @@
 function solution(n) {
-    let arr = [];
+    // let arr = [];
+    let sum = 0;
     
     for(let i=1; i<=(Math.sqrt(n)); i++){
         if(n%i==0){
-            arr.push(i);
+            // arr.push(i);
+            sum += i;
             if(i !== n/i){
-                arr.push(n/i);
+                // arr.push(n/i);
+                sum += n/i;
             }
         }
     }
-    const sum = arr.reduce((acc,cur)=> acc + cur, 0);
-    console.log(sum);
-    return sum
+    // const sum = arr.reduce((acc,cur)=> acc + cur, 0);
+    // console.log(sum);
+    return sum;
 }
